75cb697f277c895cfd891cfd58c07f40fc010789
mango <esudatcom@gmail.com>
2023-12-27 22:41:29 +0900

    서블릿 요청 응답 보내기

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -16,5 +16,13 @@ public class HelloServlet extends HttpServlet {
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/planin");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello"+username);
+
     }
 }
