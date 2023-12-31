5e25dedbb63e97ea2a33ef95ca4f6f60b8b61865
mango <esudatcom@gmail.com>
2023-12-31 16:55:44 +0900

    get 쿼리 파라미터 연습 - 단일 파라미터 조회

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -21,5 +21,13 @@ public class RequestParamServlet extends HttpServlet {
         request.getParameterNames().asIterator()
                         .forEachRemaining(paramName -> System.out.println(paramName = "=" + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
     }
 }
